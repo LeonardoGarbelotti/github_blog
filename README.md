6312fa25055207f9d29c9c7b6e7340993b175ce5
Leonardo Gonçalves <leo13garbelotti@hotmail.com>
2023-07-18 13:25:35 -0300

    add readme and other features

---
+++ README.md
@@ -0,0 +1,26 @@
+# Github Blog
+
+<img src="./src/assets/screenshots/readme-banner.png" align="center">
+
+O Github blog é um desafio proposto no 3º módulo da trilha React da RocketSeat. A aplicação consiste em um blog utilizando a API do Github para buscar Issues de um repositório.
+
+## 🛠️ Nesse projeto foi utilizado
+* Vite
+* Styled Components
+* Typescript
+* React Router Dom
+* React Hook Form
+* Axios
+* API do Github
+
+## 📷 Screenshots
+Imagens da aplicação
+
+### Página Inicial
+
+<img src="./src/assets/screenshots/app1.png" align="center">
+
+
+### Página conteúdo do Post
+
+<img src="./src/assets/screenshots/app2.png" align="center">
--- src/pages/Post/components/PostHeader/index.tsx
+++ src/pages/Post/components/PostHeader/index.tsx
@@ -8,39 +8,60 @@ import {
   faComment,
 } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from 'react-router-dom'
+import { IPost } from '../../../Home'
+import { Spinner } from '../../../../components/Spinner'
+import { DateFormatter } from '../../../../utils/formatter'
 
-export function PostHeader() {
+interface PostHeaderProps {
+  postData: IPost
+  isLoading: boolean
+}
+
+export function PostHeader({ postData, isLoading }: PostHeaderProps) {
   const navigate = useNavigate()
 
   function goBack() {
     navigate('/')
   }
 
+  const formatedDate = DateFormatter(postData?.created_at)
+
   return (
     <PostHeaderContainer>
-      <header>
-        <ExternalLink
-          text="Voltar"
-          onClick={goBack}
-          icon={<FontAwesomeIcon icon={faChevronLeft} />}
-          variant="iconLeft"
-        />
-        <ExternalLink text="Ver no Github" href="#" target="_blank" />
-      </header>
-      <h1>JavaScript data types and data structures</h1>
-      <ul>
-        <li>
-          <FontAwesomeIcon icon={faGithub} />
-          LeonardoGarbelotti
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faCalendar} />
-          Há 1 dia
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faComment} />5 comentários
-        </li>
-      </ul>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <header>
+            <ExternalLink
+              text="Voltar"
+              onClick={goBack}
+              icon={<FontAwesomeIcon icon={faChevronLeft} />}
+              variant="iconLeft"
+            />
+            <ExternalLink
+              text="Ver no Github"
+              href={postData.html_url}
+              target="_blank"
+            />
+          </header>
+          <h1>{postData.title}</h1>
+          <ul>
+            <li>
+              <FontAwesomeIcon icon={faGithub} />
+              {postData.user.login}
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faCalendar} />
+              {formatedDate}
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faComment} />
+              {postData.comments} comentários
+            </li>
+          </ul>
+        </>
+      )}
     </PostHeaderContainer>
   )
 }
